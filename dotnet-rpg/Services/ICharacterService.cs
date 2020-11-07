2f2b2e30e9e9b3cd848d097f7154d9c8c613bde7
Elad Ezra <xxeladxx@gmail.com>
2020-11-07 17:27:43 +0200

    - Almost finished with the web api core   everything is working fine.

---
+++ dotnet-rpg/Services/ICharacterService.cs
@@ -0,0 +1,18 @@
+﻿using dotnet_rpg.Dtos.Character;
+using dotnet_rpg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services
+{
+    public interface ICharacterService
+    {
+        Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters();
+
+        Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
+
+        Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto character);
+    }
+}
